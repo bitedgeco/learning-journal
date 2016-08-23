ef77701c6e710d52f8a49807ad68ab9f984748f4
James <canning.jimmy@gmail.com>
2016-08-23 10:09:14 -0700

    changed run

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -2,3 +2,6 @@ def includeme(config):
     """ This function adds routes to Pyramid's Configurator """
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+
+
+    # config.add_route('new-entry', '/new-entry')
