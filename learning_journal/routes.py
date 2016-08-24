b88fdc1a66ccde059dbf4b3a95ee72ca834378fe
James <canning.jimmy@gmail.com>
2016-08-23 19:52:56 -0700

    Basic routs and views working localy

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -2,7 +2,11 @@ def includeme(config):
     """ This function adds routes to Pyramid's Configurator """
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('detail', '/detail')
+    config.add_route('edit', '/edit')
+    config.add_route('new', '/new')
 
 
-    # config.add_route('new-entry', '/new-entry') 
-    # config.add_route('detail', '/detail/{id:\d+}')
+
+    # config.add_route('edit', '/edit/{id:\d+}')
+
--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,17 +1,32 @@
 from pyramid.response import Response
 import os
 
-# def home_page(request):
-#     return Response("This is my first view!")
-
 
 def includeme(config):
-    config.add_view(home_page, route_name='home')
+    config.add_view(home, route_name='home')
+    config.add_view(detail, route_name='detail')
+    config.add_view(edit, route_name='edit')
+    config.add_view(new, route_name='new')
 
 
 HERE = os.path.dirname(__file__)
 
 
-def home_page(request):
-    imported_text = open(os.path.join(HERE, 'template/home.html')).read()
+def home(request):
+    imported_text = open(os.path.join(HERE, 'templates/home.html')).read()
+    return Response(imported_text)
+
+
+def detail(request):
+    imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
+    return Response(imported_text)
+
+
+def edit(request):
+    imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
+    return Response(imported_text)
+
+
+def new(request):
+    imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
     return Response(imported_text)
