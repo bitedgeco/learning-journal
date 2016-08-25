c7183cf1e0a5efddbb66e4db3c5745d356515551
James <canning.jimmy@gmail.com>
2016-08-24 19:52:23 -0700

    front end tests passing

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -1,29 +1,36 @@
-import unittest
+import pytest
+
 
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'learning-journal')
+def test_detail_view():
+    '''Checks respons to reques with id 13 contains a title'''
+    from .views import detail_view
+    request = testing.DummyRequest()
+    request.matchdict['id'] = 13
+    info = detail_view(request)
+    assert "title" in info
 
 
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from learning_journal import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
+@pytest.fixture()
+def testapp():
+    '''makes test app for testing'''
+    from learning_journal import main
+    app = main({})
+    from webtest import TestApp
+    return TestApp(app)
 
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+
+def test_layout_root(testapp):
+    '''Test body contains paragraph tags'''
+    response = testapp.get('/', status=200)
+    assert b'<p>' in response.body
+
+
+def test_root_contents(testapp):
+    '''Test that the contents of the root page contains as many <h2> tags as journal entries.'''
+    from .views import ENTRIES
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(ENTRIES) == len(html.findAll("h2"))
